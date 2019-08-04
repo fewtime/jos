d7160dd2da8957a8d6bb5d12bb8cffc48f3a4483
cowlog <cowlog.li@gmail.com>
2019-08-04 12:05:57 +0800

    [lab1] Bugfix: backtrace instruction
    backtrace call ~mon_backtrace()~ instead of ~mon_kerninfo()~

--- kern/monitor.c
+++ kern/monitor.c
@@ -25,7 +25,7 @@ struct Command {
 static struct Command commands[] = {
     {"help", "Display this list of commands", mon_help},
     {"kerninfo", "Display information about the kernel", mon_kerninfo},
-    {"backtrace", "Display a backtrace of the function stack", mon_kerninfo},
+    {"backtrace", "Display a backtrace of the function stack", mon_backtrace},
 };
 
 /***** Implementations of basic kernel monitor commands *****/
