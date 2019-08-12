658947e8c6d4e4022409eb2b79b2ed4e36d7bd30
cowlog <cowlog.li@gmail.com>
2019-08-13 00:04:58 +0800

    [lab4] Exercise 11 completed.

--- lib/pgfault.c
+++ lib/pgfault.c
@@ -29,8 +29,17 @@ set_pgfault_handler(void (*handler)(struct UTrapframe *utf))
 	if (_pgfault_handler == 0) {
 		// First time through!
 		// LAB 4: Your code here.
-		panic("set_pgfault_handler not implemented");
-	}
+
+                // 当前进程分配异常栈
+		if (sys_page_alloc(0, (void *)(UXSTACKTOP - PGSIZE), PTE_W |PTE_U | PTE_P) < 0) {
+			panic("set_pgfault_handler: sys_page_alloc() failed.");
+		}
+
+                // 系统调用，设置进程 env_pgfault_upcall
+		if (sys_env_set_pgfault_upcall(0, _pgfault_upcall) < 0) {
+			panic("set_pgfault_handler: bad env.");
+		}
+        }
 
 	// Save handler pointer for assembly to call.
 	_pgfault_handler = handler;
