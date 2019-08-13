03f12b92d7b4da9102d7738312288ea7900e470a
cowlog <cowlog.li@gmail.com>
2019-08-13 16:58:19 +0800

    [lab4] Fix Bugs: assert(!read_eflags() & FL_IF)
    ~SETGATE()~ wrong ~istrap~ from Lab 3 Exercise 4,
    which cause the assert error

--- kern/trap.c
+++ kern/trap.c
@@ -89,23 +89,23 @@ trap_init(void)
         void fperr_entry();
         void syscall_entry();
 
-	SETGATE(idt[T_DIVIDE], 1, GD_KT, divide_entry, 0);
-	SETGATE(idt[T_DEBUG], 1, GD_KT, debug_entry, 3);
-        SETGATE(idt[T_NMI], 1, GD_KT, nmi_entry, 0);
-        SETGATE(idt[T_BRKPT], 1, GD_KT, brkpt_entry, 3);
-        SETGATE(idt[T_OFLOW], 1, GD_KT, oflow_entry, 0);
-        SETGATE(idt[T_BOUND], 1, GD_KT, bound_entry, 0);
-        SETGATE(idt[T_ILLOP], 1, GD_KT, illop_entry, 0);
-        SETGATE(idt[T_DEVICE], 1, GD_KT, device_entry, 0);
-        SETGATE(idt[T_DBLFLT], 1, GD_KT, dblflt_entry, 0);
+	SETGATE(idt[T_DIVIDE], 0, GD_KT, divide_entry, 0);
+	SETGATE(idt[T_DEBUG], 0, GD_KT, debug_entry, 3);
+        SETGATE(idt[T_NMI], 0, GD_KT, nmi_entry, 0);
+        SETGATE(idt[T_BRKPT], 0, GD_KT, brkpt_entry, 3);
+        SETGATE(idt[T_OFLOW], 0, GD_KT, oflow_entry, 0);
+        SETGATE(idt[T_BOUND], 0, GD_KT, bound_entry, 0);
+        SETGATE(idt[T_ILLOP], 0, GD_KT, illop_entry, 0);
+        SETGATE(idt[T_DEVICE], 0, GD_KT, device_entry, 0);
+        SETGATE(idt[T_DBLFLT], 0, GD_KT, dblflt_entry, 0);
 
-        SETGATE(idt[T_TSS], 1, GD_KT, tss_entry, 0);
-        SETGATE(idt[T_SEGNP], 1, GD_KT, segnp_entry, 0);
-        SETGATE(idt[T_STACK], 1, GD_KT, stack_entry, 0);
-        SETGATE(idt[T_GPFLT], 1, GD_KT, gpflt_entry, 0);
-        SETGATE(idt[T_PGFLT], 1, GD_KT, pgflt_entry, 0);
-        SETGATE(idt[T_FPERR], 1, GD_KT, fperr_entry, 0);
-        SETGATE(idt[T_SYSCALL], 1, GD_KT, syscall_entry, 3);
+        SETGATE(idt[T_TSS], 0, GD_KT, tss_entry, 0);
+        SETGATE(idt[T_SEGNP], 0, GD_KT, segnp_entry, 0);
+        SETGATE(idt[T_STACK], 0, GD_KT, stack_entry, 0);
+        SETGATE(idt[T_GPFLT], 0, GD_KT, gpflt_entry, 0);
+        SETGATE(idt[T_PGFLT], 0, GD_KT, pgflt_entry, 0);
+        SETGATE(idt[T_FPERR], 0, GD_KT, fperr_entry, 0);
+        SETGATE(idt[T_SYSCALL], 0, GD_KT, syscall_entry, 3);
 
 	// irq
 	void irq_handler0();
