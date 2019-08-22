85f6a2510d06e53dab1702aff62cb1a675cf8824
cowlog <cowlog.li@gmail.com>
2019-08-22 10:19:23 +0800

    [lab6] Exercise 4 completed.

--- kern/e1000.c
+++ kern/e1000.c
@@ -6,5 +6,10 @@
 int
 e1000_attachfn(struct pci_func *pcif) {
 	pci_func_enable(pcif);
+	cprintf("reg_base: %x, reg_size: %x\n", pcif->reg_base[0], pcif->reg_size[0]);
+
+	// memory mapping
+	mmio_e1000 = mmio_map_region((physaddr_t)pcif->reg_base[0], (size_t)pcif->reg_size[0]);
+	assert(mmio_e1000[E1000_STATUS] == 0x80080783);
 	return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -6,7 +6,12 @@
 #define E1000_VENDOR_ID_82540EM 0x8086
 #define E1000_DEVICE_ID_82540EM 0x100e
 
+/* Register Set. (82543, 82544) */
+#define E1000_STATUS 0x00008 / 4 /* Device Status - RO */
+
 int e1000_attachfn(struct pci_func *pcif);
+
+volatile uint32_t *mmio_e1000;
 
 #endif  // SOL >= 6
 
