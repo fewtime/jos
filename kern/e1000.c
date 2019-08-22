e7b82a99ee39a7d0e98da2177b1c3a2426037016
cowlog <cowlog.li@gmail.com>
2019-08-22 11:05:56 +0800

    [lab6] Exercise 5 completed.

--- kern/e1000.c
+++ kern/e1000.c
@@ -3,13 +3,46 @@
 
 // LAB 6: Your driver code here
 
-int
-e1000_attachfn(struct pci_func *pcif) {
-	pci_func_enable(pcif);
-	cprintf("reg_base: %x, reg_size: %x\n", pcif->reg_base[0], pcif->reg_size[0]);
+struct e1000_tx_desc tx_descs[TXRING_LEN]
+    __attribute__((aligned(PGSIZE))) = {0};
+char tx_buffers[TXRING_LEN][TX_PACKAGE_SIZE]
+    __attribute__((aligned(PGSIZE))) = {0};
 
-	// memory mapping
-	mmio_e1000 = mmio_map_region((physaddr_t)pcif->reg_base[0], (size_t)pcif->reg_size[0]);
-	assert(mmio_e1000[E1000_STATUS] == 0x80080783);
-	return 0;
+int e1000_attachfn(struct pci_func *pcif) {
+  pci_func_enable(pcif);
+  init_desc();
+  cprintf("reg_base: %x, reg_size: %x\n", pcif->reg_base[0], pcif->reg_size[0]);
+
+  // memory mapping
+  mmio_e1000 =
+      mmio_map_region((physaddr_t)pcif->reg_base[0], (size_t)pcif->reg_size[0]);
+
+  e1000_transmit_init();
+  return 0;
+}
+
+static void init_desc(void) {
+  int i = 0;
+  for (i = 0; i < TXRING_LEN; ++i) {
+    memset(&tx_descs[i], 0, sizeof(struct e1000_tx_desc));
+    memset(&tx_buffers[i], 0, TX_PACKAGE_SIZE);
+    tx_descs[i].addr = PADDR(&tx_buffers[i]);
+    tx_descs[i].status = E1000_TXD_STAT_DD;
+  }
+}
+
+int e1000_transmit_init(void) {
+  assert(mmio_e1000[E1000_STATUS] == 0x80080783);
+  mmio_e1000[E1000_TDBAL] = PADDR(tx_descs);
+  mmio_e1000[E1000_TDBAH] = 0;
+  mmio_e1000[E1000_TDLEN] = VALUEMASK(TXRING_LEN, E1000_TDLEN_LEN);
+  mmio_e1000[E1000_TDH] = 0;
+  mmio_e1000[E1000_TDT] = 0;
+  mmio_e1000[E1000_TCTL] =
+      VALUEMASK(1, E1000_TCTL_EN) | VALUEMASK(1, E1000_TCTL_PSP) |
+      VALUEMASK(0x10, E1000_TCTL_CT) | VALUEMASK(0x40, E1000_TCTL_COLD);
+  mmio_e1000[E1000_TIPG] = VALUEMASK(0x10, E1000_TIPG_IPGT) |
+			   VALUEMASK(0x4, E1000_TIPG_IPGR1) |
+			   VALUEMASK(0x6, E1000_TIPG_IPGR2);
+  return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,6 +1,7 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#include "inc/string.h"
 #include "kern/pci.h"
 
 #define E1000_VENDOR_ID_82540EM 0x8086
@@ -8,10 +9,50 @@
 
 /* Register Set. (82543, 82544) */
 #define E1000_STATUS 0x00008 / 4 /* Device Status - RO */
+#define E1000_TDBAL 0x03800 / 4  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH 0x03804 / 4  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN 0x03808 / 4  /* TX Descriptor Length - RW */
+#define E1000_TDH 0x03810 / 4    /* TX Descriptor Head - RW */
+#define E1000_TDT 0x03818 / 4    /* TX Descripotr Tail - RW */
+#define E1000_TCTL 0x00400 / 4   /* TX Control - RW */
+#define E1000_TIPG 0x00410 / 4   /* TX Inter-packet gap -RW */
+
+/* Transmit Descriptor Length*/
+#define E1000_TDLEN_LEN 0x000fff80
+
+/* Transmit Control */
+#define E1000_TCTL_EN 0x00000002   /* enable tx */
+#define E1000_TCTL_PSP 0x00000008  /* pad short packets */
+#define E1000_TCTL_CT 0x00000ff0   /* collision threshold */
+#define E1000_TCTL_COLD 0x003ff000 /* collision distance */
+
+/* TX Inter-packet gap bit definitions */
+#define E1000_TIPG_IPGT 0x000003ff
+#define E1000_TIPG_IPGR1 0x000ffc00
+#define E1000_TIPG_IPGR2 0x3ff00000
+
+/* Transmit Descriptor bit definitions */
+#define E1000_TXD_STAT_DD 0x00000001 /* Descriptor Done */
+
+#define TXRING_LEN 32
+#define TX_PACKAGE_SIZE 1518
+
+#define VALUEMASK(value, mask) (value) * ((mask) & ~((mask) << 1))
+
+struct e1000_tx_desc {
+  uint64_t addr;
+  uint16_t length;
+  uint8_t cso;
+  uint8_t cmd;
+  uint8_t status;
+  uint8_t css;
+  uint16_t special;
+} __attribute__((packed));
 
 int e1000_attachfn(struct pci_func *pcif);
+static void init_desc(void);
+int e1000_transmit_init(void);
 
 volatile uint32_t *mmio_e1000;
 
-#endif  // SOL >= 6
-
+#endif // SOL >= 6
