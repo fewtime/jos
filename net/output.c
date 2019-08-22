dc9c83c7e8f3e74fa69cab6956c91e5dad0dce29
cowlog <cowlog.li@gmail.com>
2019-08-22 17:34:34 +0800

    [lab6] Exercise 8 completed.

--- net/output.c
+++ net/output.c
@@ -1,3 +1,4 @@
+#include "inc/lib.h"
 #include "ns.h"
 
 extern union Nsipc nsipcbuf;
@@ -10,4 +11,17 @@ output(envid_t ns_envid)
 	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
+	envid_t envid = 0;
+        int perm = 0;
+
+        while (1) {
+		if (ipc_recv((envid_t *)&envid, &nsipcbuf, &perm) != NSREQ_OUTPUT) {
+			continue;
+		}
+
+
+		while (sys_package_send((void *)nsipcbuf.pkt.jp_data, (size_t)nsipcbuf.pkt.jp_len) < 0) {
+			sys_yield();
+		}
+	}
 }
